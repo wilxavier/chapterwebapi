dec960d98ce0bb7b8cf97a807936205d6c5729c8
William Xavier <william.santos@aluno.unb.br>
2024-11-24 00:52:20 -0300

    swagger

--- Program.cs
+++ Program.cs
@@ -1,18 +1,38 @@
 using Chapter.WebApi.Contexts;
 using Chapter.WebApi.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<ChapterContext,ChapterContext>();
+builder.Services.AddScoped<ChapterContext, ChapterContext>();
 builder.Services.AddControllers();
 builder.Services.AddTransient<LivroRepository, LivroRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "ChapterApi",
+            Version = "v1"
+        });
+
+    });
+
 var app = builder.Build();
 
+app.UseDeveloperExceptionPage();
+//ativa o middleware para uso do swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChapterApi v1"));
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
